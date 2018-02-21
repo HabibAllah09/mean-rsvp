cacc8389ccb4f10289e7521f1c722435074e499f
HabibAllah <habib891allah@gmail.com>
2018-02-21 18:08:53 +0200

    "Add Node, Mongo configurations + home, header and footer components"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'World';
+  myWord = 'One Piece Does Exist !'
 }
